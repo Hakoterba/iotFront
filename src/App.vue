b8b033bef231a55f7a61228b88dafe7128a9881e
Tran-Hugo <82827738+Tran-Hugo@users.noreply.github.com>
2025-02-25 16:36:40 +0100

    Revert "Merge branch 'hugo-dev' of https://github.com/Hakoterba/iotFront"
    This reverts commit 7d5c6b50db19e84f6668d6e28fb440711a745b5d, reversing
    changes made to e42c4e5e501e0c12ebb6b54bd78a2ae8cfb6cfbc.

--- src/App.vue
+++ src/App.vue
@@ -1,68 +1,7 @@
 <template>
-<<<<<<< HEAD
   <FlagCanvas />
 </template>
 
 <script setup lang="ts">
 import FlagCanvas from '../src/components/FlagCanva.vue';
-=======
-  <Scores ref="score" />
-  <button @click="startGame">Start game</button>
-  <button @click="resetGame">Reset</button>
- 
-</template>
-
-<script>
-import { io } from "socket.io-client";
-import Scores from './components/Scores.vue'
-
-export default {
-  components: {
-    Scores
-  },
-  data() {
-    return {
-      stageSize: {
-        width: window.innerWidth,
-        height: window.innerHeight
-      },
-      isDragging: false,
-      image: null,
-      socket: null
-    };
-  },
-  methods: {
-    startGame() {
-        this.socket.emit('start_game');
-    },
-    resetGame() {
-        this.socket.emit('reset_game');
-    }
-  },
-  mounted() {
-    console.log('Connexion au serveur WebSocket');
-    
-    this.socket = io('http://lasergameserver.hugotran.fr:3001');
-
-    this.socket.on('connect', () => {
-                console.log('Connecté au serveur WebSocket');
-                this.socket.emit('client_data', {id: 'client'});
-            });
-
-    this.socket.on('message', (data) => {
-        console.log('Message du serveur:', data);
-    });
-
-    this.socket.on("flags", (data) => {
-        console.log('Flags:', data);
-    });
-
-    this.socket.on("state", (data) => {
-      console.log('State:', this.$refs.score);
-      
-        this.$refs.score.updateValue(JSON.parse(data))
-    });
-  }
-};
->>>>>>> 2f320142fb801ecd0394fe6dc63df9eacf374760
 </script>
