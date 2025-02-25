b6923c7716e088ec3b5586ff39f32d2b13fd3399
Tran-Hugo <82827738+Tran-Hugo@users.noreply.github.com>
2025-02-25 15:43:28 +0100

    add websocket connexion, add scores component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,48 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import Scores from './components/Scores.vue'
+import { io } from "socket.io-client";
+import { useTemplateRef, onMounted } from 'vue'
+
+const score = useTemplateRef("score");
+const socket = io('http://lasergameserver.hugotran.fr:3001');
+
+socket.on('connect', () => {
+            console.log('Connecté au serveur WebSocket');
+            socket.emit('client_data', {id: 'client'});
+        });
+
+socket.on('message', (data) => {
+    console.log('Message du serveur:', data);
+});
+
+function startGame() {
+    socket.emit('start_game');
+}
+
+function resetGame() {
+    socket.emit('reset_game');
+}
+
+socket.on("flags", (data) => {
+    console.log('Flags:', data);
+});
+
+socket.on("state", (data) => {
+    score.value.updateValue(JSON.parse(data))
+});
+
 </script>
 
 <template>
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
+    <h1>Lasergame</h1>
+    <button @click="startGame">Start game</button>
+    <button @click="resetGame">Reset</button>
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <Scores ref="score"/>
 </template>
 
 <style scoped>
---
+++ src/components/Scores.vue
@@ -0,0 +1,49 @@
+<template>
+    <div style="width: 100%">
+        <LvProgressBar :value="redScore" :showValue="false" color="red" />
+        <br />
+        <LvProgressBar :value="blueScore" :showValue="false" color="blue" />
+    </div>
+</template>
+
+<script>
+import LvProgressBar from 'lightvue/progress-bar';
+
+export default {
+    data() {
+        return {
+            redScore: 0,
+            blueScore: 0,
+            trueScores: {
+                redScore: 0,
+                blueScore: 0
+            }
+        }
+    },
+    interval: null,
+    methods: {
+        endProgress() {
+            clearInterval(this.interval);
+            this.interval = null;
+        },
+        updateValue(newValues){
+            this.trueScores.redScore = newValues.score_red;
+            this.trueScores.blueScore = newValues.score_blue;
+            this.redScore = (newValues.score_red / 300) * 100;
+            this.blueScore = (newValues.score_blue / 300) * 100;
+        }
+    },
+    mounted() {
+    },
+    beforeDestroy() {
+        this.endProgress();
+    },
+    components: {
+        LvProgressBar: LvProgressBar,
+    }
+}
+</script>
+
+<style>
+
+</style>
